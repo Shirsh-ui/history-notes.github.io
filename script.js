6ae157a5821c3290f634e89ddc558c0089e05be3
Shirsh Krejia <63437916+Shirsh-ui@users.noreply.github.com>
2021-09-07 18:01:57 +0530

    styling in js

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ customElements.define('point-list', class extends HTMLElement {
         let container = document.createElement('div');
         let head = document.createElement('div');
         let content = document.createElement('div');
-        let h1 = document.createElement('h3');
+        let h3 = document.createElement('h3');
         let p = document.createElement('p');
 
         let content1 = this.children[0].innerHTML;
         let content2 = this.children[1].innerHTML;
 
-        h1.innerHTML = content1;
+        h3.innerHTML = content1;
         p.innerHTML = content2;
-        head.appendChild(h1);
+
+        h3.style.paddingTop = "10px";
+        h3.style.paddingBottom = "10px";
+
+        head.appendChild(h3);
         content.appendChild(p);
         container.appendChild(head);
         container.appendChild(content);
