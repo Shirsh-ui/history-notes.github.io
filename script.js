fd7dad17811ac7aa9b3879f4ea46087f896b5dd1
Shirsh Krejia <63437916+Shirsh-ui@users.noreply.github.com>
2021-09-09 07:51:07 +0530

    corrected js code

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 customElements.define('point-list', class extends HTMLElement {
-    constructor () {
-        super();
-        let shadow = this.attachShadow({mode: 'open'});
-        let container = document.createElement('div');
-        let head = document.createElement('div');
-        let content = document.createElement('div');
-        let h3 = document.createElement('h3');
-
-        content.innerHTML = this.children[1].innerHTML;
-
-        h3.style.marginTop = "10px";
-        h3.style.marginBottom = "10px";
-
-        head.appendChild(h3);
-        container.appendChild(head);
-        container.appendChild(content);
-        shadow.appendChild(container);
+    constructor() {
+      super();
+      let shadow = this.attachShadow({
+        mode: 'open'
+      });
+      let container = document.createElement('div');
+      let head = document.createElement('div');
+      let content = document.createElement('div');
+      let h3 = document.createElement('h3');
+  
+          h3.innerHTML = this.children[0].innerHTML;
+      content.innerHTML = this.children[1].innerHTML;
+  
+      h3.style.marginTop = "10px";
+      h3.style.marginBottom = "10px";
+  
+      head.appendChild(h3);
+      container.appendChild(head);
+      container.appendChild(content);
+      shadow.appendChild(container);
     }
-});
+  });
+  
